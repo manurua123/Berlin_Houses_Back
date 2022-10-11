75cc0100866869b59a9eb45d91967a5872c35ad3
Manuel Rua <manu.rua.123@gmail.com>
2022-10-11 11:04:42 -0300

    conectar con db

---
+++ src/test/java/com/berlin_houses/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berlin_houses.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
